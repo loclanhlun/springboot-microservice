0605b58e77095fc8e83026a529dc9178e6f9a01a
Huynh Bao Loc <loclanhlun6666@gmail.com>
2021-03-09 17:31:56 +0700

    commit 3/9/2021

---
+++ src/main/java/com/huynhbaoloc/demo/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.huynhbaoloc.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+	
+	@PostMapping("/user")
+	
+}
---
+++ src/main/java/com/huynhbaoloc/demo/dto/UserDTO.java
@@ -0,0 +1,97 @@
+package com.huynhbaoloc.demo.dto;
+
+
+public class UserDTO {
+	
+	private Long id;
+	
+	private String firstName;
+	
+	private String middleName;
+	
+	private String lastName;
+	
+	private String phoneNumber;
+	
+	private String email;
+
+	private String currentAddress;
+	
+	private String company;
+	
+	private String password;
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCurrentAddress() {
+		return currentAddress;
+	}
+
+	public void setCurrentAddress(String currentAddress) {
+		this.currentAddress = currentAddress;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
